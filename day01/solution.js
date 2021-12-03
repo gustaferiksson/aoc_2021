5be74e10e0f69b72cf010e2db11c55b2dccb14d1
Gustaf Eriksson <mail@gustaf.dev>
2021-12-03 23:46:14 +0100

    day 01 done

--- day01/solution.js
+++ day01/solution.js
@@ -1,4 +1,7 @@
-const input = require("fs").readFileSync("input.txt", "utf-8").split("\n").map(Number);
+const input = require("fs").readFileSync(`${__dirname}/input.txt`, "utf-8").split("\n").map(Number);
 
-process.env.part == "part1" && console.log("solution1");
-process.env.part == "part2" && console.log("solution2");
+const s1 = input.filter((value, i) => value < input[i + 1]).length;
+const s2 = input.filter((value, i) => input.slice(i, i + 3).reduce((a, b) => a + b, 0) < input.slice(i + 1, i + 4).reduce((a, b) => a + b, 0)).length;
+
+process.env.part == "part1" && console.log(s1);
+process.env.part == "part2" && console.log(s2);
