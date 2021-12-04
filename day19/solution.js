0169f371d513d372400bb81a30d4ce91ad336453
Gustaf Eriksson <mail@gustaf.dev>
2021-12-04 01:15:40 +0100

    fixed input filename in example solution

--- day19/solution.js
+++ day19/solution.js
@@ -1,4 +1,4 @@
-const input = require("fs").readFileSync("input.txt", "utf-8").split("\n").map(Number);
+const input = require("fs").readFileSync(`${__dirname}/input.txt`, "utf-8").split("\n").map(Number);
 
 process.env.part == "part1" && console.log("solution1");
 process.env.part == "part2" && console.log("solution2");
