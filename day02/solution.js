339cff588a6f4cb278dc81e6fe885c660931ae73
Gustaf Eriksson <mail@gustaf.dev>
2021-12-04 01:06:09 +0100

    day 02 done

--- day02/solution.js
+++ day02/solution.js
@@ -1,4 +1,20 @@
-const input = require("fs").readFileSync("input.txt", "utf-8").split("\n").map(Number);
+const input = require("fs").readFileSync(`${__dirname}/input.txt`, "utf-8").split("\n");
 
-process.env.part == "part1" && console.log("solution1");
-process.env.part == "part2" && console.log("solution2");
+const isPart2 = process.env.part == "part2";
+
+let depth = 0;
+let distance = 0;
+let aim = 0;
+
+input.forEach((str) => {
+  let direction = str.at(0);
+  let value = str.at(-1) * 1;
+
+  direction == "d" && (depth += value);
+  direction == "u" && (depth -= value);
+  direction == "f" && (distance += value);
+  direction == "f" && isPart2 && (aim += depth * value);
+});
+
+isPart2 && ([depth, aim] = [aim, depth]);
+console.log(distance * depth);
